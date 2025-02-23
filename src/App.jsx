78af943167cb24ac86242bb3a608a17c16a3e291
Shreyance Bodh <shreyancebodh@gmail.com>
2025-02-23 11:28:07 +0530

    heading added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
     <div className='bg-gray-900 min-h-screen w-full overflow-hidden'>
       <div className='w-full m-auto max-w-[100%] sm:p-6 pt-4 rounded-2xl bg-gray-800 shadow-lg'>
         <h2 className='text-2xl text-center text-white mb-4'>Todo App</h2>
+        <h1>Hi there!!</h1>
         <Todo/>
       </div>
     </div>
